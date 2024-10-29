df06804079a5478031a402ef4392f3c942ab5675
lkhatmullina <lenahatmullina@mail.ru>
2024-10-29 23:43:56 +0300

    practice _ 12

--- src/Main.java
+++ src/Main.java
@@ -9,43 +9,78 @@ import Practice_Project_10_2.Triangle;
 
 import java.sql.SQLOutput;
 
-public class Main {
-    public static void main(String[] args) {
-        Animal Barsik = new Cat();
-        Animal Sharik = new Dog();
+public class Main
+{
+    public static int sumArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+    if (array.length != 4 || array[0].length != 4) {
+        throw new MyArraySizeException("Размер массива должен быть 4x4.");
+    }
 
-        Bowl bowl = new Bowl(27);
+    int sum = 0;
 
-        Cat[] catArray = new Cat[3];
-
-        for (int i = 0; i < catArray.length; i++){
-            catArray[i] = new Cat();
-            catArray[i].EatFromBowl(bowl.GetAmountOfFood(), bowl);
-            System.out.println("Кот " + i + " Покушал;");
+    for (int i = 0; i < array.length; i++) {
+        for (int j = 0; j < array[i].length; j++) {
+            try {
+                sum += Integer.parseInt(array[i][j]);
+            } catch (NumberFormatException e) {
+                throw new MyArrayDataException(
+                        "Неверные данные в ячейке [" + i + "][" + j + "]: " + array[i][j]);
+            }
         }
+    }
 
-        System.out.println(bowl.GetAmountOfFood());
-        bowl.AddFood(67);
-        System.out.println(bowl.GetAmountOfFood());
+    return sum;
+}
 
+    public static void main(String[] args)
+    {
+            String[][] validArray = {
+                    {"1", "2", "3", "4"},
+                    {"5", "6", "7", "8"},
+                    {"9", "10", "11", "12"},
+                    {"13", "14", "15", "16"}
+            };
 
-        Shape circle = new Circle(3, "Yellow", "Red");
-        Shape rectangle = new Rectangle(3,5, "Brown", "Pink");
-        Shape triangle = new Triangle(5, 2, 4, "Green", "Blue");
+            String[][] invalidSizeArray = {
+                    {"1", "2", "3"},
+                    {"4", "5", "6"}
+            };
 
-        System.out.println("Круг имеет площадь: " + circle.GetArea() + " периметр: " +
-                circle.GetPerimeter() + " цвет фона: " + ((Circle) circle).GetFillColor() + " цвет заливки: " +
-                ((Circle) circle).GetBorderColor());
+            String[][] invalidDataArray = {
+                    {"1", "2", "three", "4"},
+                    {"5", "6", "7", "8"},
+                    {"9", "10", "11", "12"},
+                    {"13", "14", "15", "16"}
+            };
 
-        System.out.println("Треугольник имеет площадь: " + triangle.GetArea() + " периметр: " +
-                triangle.GetPerimeter() + " цвет фона: " + ((Triangle) triangle).GetFillColor() + " цвет заливки: " +
-                ((Triangle) triangle).GetBorderColor());
+            try {
+                System.out.println("Сумма элементов массива: " + sumArray(validArray));
+            } catch (MyArraySizeException | MyArrayDataException e) {
+                System.out.println(e.getMessage());
+            }
 
-        System.out.println("Прямоугольник имеет площадь: " + rectangle.GetArea() + " периметр: " +
-                rectangle.GetPerimeter() + " цвет фона: " + ((Rectangle) rectangle).GetFillColor() + " цвет заливки: " +
-                ((Rectangle) rectangle).GetBorderColor());
+            try {
+                System.out.println("Сумма элементов массива: " + sumArray(invalidSizeArray));
+            } catch (MyArraySizeException | MyArrayDataException e) {
+                System.out.println(e.getMessage());
+            }
 
+            try {
+                System.out.println("Сумма элементов массива: " + sumArray(invalidDataArray));
+            } catch (MyArraySizeException | MyArrayDataException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+}
 
+class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
 
+class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message) {
+        super(message);
     }
 }
